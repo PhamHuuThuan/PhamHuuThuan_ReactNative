2729a447c0aa5062b4d2cd97ac16192d01154f34
PhamHuuThuan <92664025+PhamHuuThuan@users.noreply.github.com>
2024-08-21 09:43:36 +0700

    Tuan1 JS Bai1 BMI

--- Tuan01/21004295_PhamHuuThuan_BaiTap01.js
+++ Tuan01/21004295_PhamHuuThuan_BaiTap01.js
@@ -1,3 +1,20 @@
 //Coding chanllenge #1 BMI
-var mass = 0, height = 0, bmi = 0;
+function calBMI(mass, height){
+    return mass/(height*height);
+}
 
+//Store Mark's and John's mass and height in variables
+var massMark = 78;
+var heightMark = 1.69;
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = calBMI(massMark, heightMark);
+var BMIJohn = calBMI(massJohn, heightJohn);
+
+//3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+var markHigherBMI = false;
+markHigherBMI = BMIMark > BMIJohn;
+
+//Print result
+console.log("Is Mark's BMI higher than John's? " + markHigherBMI);
