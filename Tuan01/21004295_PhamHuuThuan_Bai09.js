db16f6ef1a4a9236c135c1b15bdd1b4eef49cee3
PhamHuuThuan <92664025+PhamHuuThuan@users.noreply.github.com>
2024-08-24 22:29:50 +0700

    Tuan1 JS Bai09 Debug in VSCode

---
+++ Tuan01/21004295_PhamHuuThuan_Bai09.js
@@ -0,0 +1,9 @@
+function printForecast(arr){
+    var str = '';
+    for (var i = 0; i < arr.length; i++){
+        str += `${arr[i]}ºC in ${i+1} days... `;
+    }
+    console.log(str);
+}
+printForecast([17, 21, 23]);
+printForecast([12, 5, -5, 0, 4]);
