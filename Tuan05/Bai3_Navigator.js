91c0eb0e26b229224f912e4180282bf8a0bbce71
PhamHuuThuan <92664025+PhamHuuThuan@users.noreply.github.com>
2024-09-29 22:20:31 +0700

    Tuan05 RN Screen02 Navigator FlatList

--- Tuan05/Bai3_Navigator.js
+++ Tuan05/Bai3_Navigator.js
@@ -1,9 +1,77 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Image, Pressable, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, FlatList} from 'react-native';
 import { NavigationContainer, useNavigation} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { FontAwesome } from '@expo/vector-icons';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
+
+const DATA = [
+  {
+    id: 'P001',
+    name: 'Pinarello',
+    price: 1800,
+    discount: 15,
+    img: 'https://s.net.vn/ZArb',
+    desc: 'It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc'
+  },
+    {
+    id: 'P002',
+    name: 'Pina Mountai',
+    price: 1700,
+    discount: 20,
+    img: 'https://s.net.vn/v9et',
+    desc: 'It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc'
+  },
+  {
+    id: 'P003',
+    name: 'Pina Bike',
+    price: 1500,
+    discount: 10,
+    img: 'https://s.net.vn/dd9m',
+    desc: 'It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc'
+  },
+  {
+    id: 'P004',
+    name: 'Pinarello',
+    price: 1500,
+    discount: 30,
+    img: 'https://s.net.vn/OxZ3',
+    desc: 'It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc'
+  },
+  {
+    id: 'P005',
+    name: 'Pinarello',
+    price: 2700,
+    discount: 35,
+    img: 'https://s.net.vn/dd9m',
+    desc: 'It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc'
+  },
+  {
+    id: 'P006',
+    name: 'Pinarello',
+    price: 1350,
+    discount: 5,
+    img: 'https://s.net.vn/v9et',
+    desc: 'It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc'
+  },
+  {
+    id: 'P007',
+    name: 'Pina Bike',
+    price: 1500,
+    discount: 10,
+    img: 'https://s.net.vn/dd9m',
+    desc: 'It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc'
+  },
+  {
+    id: 'P008',
+    name: 'Pina Bike',
+    price: 1500,
+    discount: 10,
+    img: 'https://s.net.vn/dd9m',
+    desc: 'It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc'
+  }
+]
 
 function HomeScreen(){
   const [fontsLoaded, setFontsLoaded] = useState(false);
@@ -11,6 +79,7 @@ function HomeScreen(){
   const loadFonts = async () => {
     await Font.loadAsync({
       'VT323': require('/assets/VT323-Regular.ttf'), // Adjust the path if necessary
+      'Voltaire': require('/assets/Voltaire-Regular.ttf'),
     });
     setFontsLoaded(true);
   };
@@ -40,13 +109,56 @@ function HomeScreen(){
     </View>
   );
 }
+const Item = ({ item }) => {
+  const [isPressed, setIsPressed] = useState(false); // Move useState here
+
+  return (
+    <View style={styles.backgroundItem}>
+      <Pressable
+        style={[styles.backgroundItem, isPressed && styles.shadow]}
+        onPressIn={() => setIsPressed(true)}
+        onPressOut={() => setIsPressed(false)}
+      >
+        <View>
+          <FontAwesome name="heart-o" size={24} color="gray" style={{ top: 10, left: -5 }} />
+          <Image source={item.img} style={styles.imgProduct} />
+        </View>
+        <Text style={styles.nameProduct}>{item.name}</Text>
+        <Text style={styles.nameProduct}>
+          <Text style={{ color: 'rgba(247, 186, 131, 1)' }}>$</Text> {item.price}
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
 
 function ListProductScreen(){
   const navigation = useNavigation();
+  const [selected, setSelected] = useState('All');
   return (
-    <View style={styles.container, {}}>
-        <View>
+    <View style={styles.container}>
+        <View style={{paddingTop: 40}}>
           <Text style={{color: 'rgba(233, 65, 65, 1)', fontSize: 25, fontWeight: '700'}}>The world’s Best Bike</Text>
+        </View>
+        <View style={styles.filterArea}>
+          <Pressable style={styles.filterBtn} onPress={() => setSelected('All')}>
+            <Text style={{color: selected === 'All' ? 'red' : 'gray', fontSize: 20}}>All</Text>
+          </Pressable>
+          <Pressable style={styles.filterBtn} onPress={() => setSelected('Road Bike')}>
+            <Text style={{color: selected === 'Road Bike'?'red':'gray', fontSize: 20}}>Road Bike</Text>
+          </Pressable>
+          <Pressable style={styles.filterBtn} onPress={() => setSelected('Mountain')}>
+            <Text style={{color: selected === 'Mountain'?'red':'gray', fontSize: 20}}>Mountain</Text>
+          </Pressable>
+        </View>
+        <View style={styles.listProduct}>
+          <FlatList
+            data={DATA}
+            renderItem={({item})=><Item item = {item}/>}
+            numColumns={2}
+            showsVerticalScrollIndicator={false}>
+          </FlatList>
         </View>
     </View>
   );
@@ -57,7 +169,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer independent={true}>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="ListProduct">
         <Stack.Screen 
           name="Home" 
           component={HomeScreen} 
@@ -80,7 +192,7 @@ const styles = StyleSheet.create({
     padding: 8,
     justifyContent: 'flex-start',
     textAlign: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   imageProduct:{
     width: 292,
@@ -105,5 +217,51 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 100,
     marginBottom: 50
+  },
+  filterArea:{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 30,
+    paddingBottom: 15
+  },
+  filterBtn:{
+    width: 99,
+    height: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'rgba(233, 65, 65, 0.53)',
+    borderRadius: 5,
+    margin: 5
+  },
+  imgProduct:{
+    width: 135,
+    height: 127
+  },
+  listProduct:{
+    flex: 1
+  },
+  backgroundItem:{
+    position: 'relative',
+    backgroundColor: 'rgba(247, 186, 131, 0.15)',
+    width: 167,
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    margin: 10
+  },
+  nameProduct:{
+    fontSize: 20,
+    fontFamily: 'Voltaire',
+    color: 'rgba(0, 0, 0, 0.6)'
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 5,
   }
 });
